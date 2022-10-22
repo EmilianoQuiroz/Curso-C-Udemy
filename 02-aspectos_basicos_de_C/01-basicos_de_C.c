ef3f370bc21bbce443e27a76f6e9c3a87f85b21e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-10-22 19:15:03 -0300

    Uso de variables en C

---
+++ 02-aspectos_basicos_de_C/01-basicos_de_C.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //Variables en C
+    //char sirve para representar una variable de tipo caracter
+    char nombre[] = "Santiago";
+    //int sirve para representar una variable de tipo entero
+    int edad = 40;
+    printf("Soy %s y tengo %d años.\n",nombre, edad);
+    return 0;
+}
