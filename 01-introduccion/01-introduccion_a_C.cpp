bfc83f408611f96b841c9723b034f592970cb1f5
Emiliano Quiroz <santiagoquiroz.dev@gmail.com>
2022-10-22 18:23:40 -0300

    Introduccion a C

---
+++ 01-introduccion/01-introduccion_a_C.cpp
@@ -0,0 +1,12 @@
+/*Curso de C Udemy: de basico a experto*/
+
+/*Estructura basica de un proyecto en C*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{		//Este programa escribe la frase Hola Mundo en la consola de comandos
+		printf("Hola mundo!\n");
+		return 0;
+}
