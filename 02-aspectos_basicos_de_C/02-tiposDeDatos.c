42bf6fc8e44e89924591e63f6d308cf4a9f911c7
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-08 16:17:04 -0300

    Tipos de datos en C

---
+++ 02-aspectos_basicos_de_C/02-tiposDeDatos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    //Tipos de datos en C
+    int numeroEntero = 10;
+    float numeroFlotante = 2.3;
+    double numeroDecimal = 3.4;
+    char letra = 'A';
+    char frase[] = "Hola";
+    return 0;
+}
