67568d2f07fc19b81c949e6ef648b689b5771f5f
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-09 17:33:36 -0300

    Ciclos iterativos

--- 05-bucles_iterativos_en_C/01-bucle_while.c
+++ 05-bucles_iterativos_en_C/01-bucle_while.c
@@ -3,6 +3,27 @@
 
 int main(){
     //Bucle While
+    //En el bucle while primero se cumple la condicion y luego se ejecuta el codigo
+    int i = 1;
+    //Mientras i sea menor o igual a 10
+    // while (i<=10)
+    // {
+    //     /* se ejecuta este codigo*/
+    //     printf("%d\n",i);
+
+    //     i = i + 1;
+    // }
+
+    //Bucle do while
+    //Primero ejecuto el codigo y luego pregunta la condicion
+    do{
+        printf("%d\n",i);
+
+        i = i +1;
+
+    }while(i<=10);
+
+    
     
     return 0;
 }
---
+++ 05-bucles_iterativos_en_C/02-bucle_for.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //Bucle for
+    
+    
+    return 0;
+}
