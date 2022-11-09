46d034eb801cb2cfd8c18c6fe51817e42475fdfc
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-09 14:25:38 -0300

    Estructuras en C

--- 02-aspectos_basicos_de_C/05-solicitarIformacion.c
+++ 02-aspectos_basicos_de_C/05-solicitarIformacion.c
@@ -6,15 +6,10 @@ int main(){
     char letra;
     //Primero le decimos al usuario que dato queremos que ingrese
     printf("Introduce cuanto dinero tenes: ");
-    printf("Introduce una letra: \n");
-
     //Luego guardamos esos datos
     scanf("%d", &dinero);
-    scanf("%c", &letra);
-
     //Por ultimo mostramos el dato por pantalla
     printf("Tienens %d \n", dinero); 
-    printf("Tienens %c \n", letra);
     //Podemos pedir y mostrar cualquier tipo de dato
 
     return 0;
--- 02-aspectos_basicos_de_C/06-arrays.c
+++ 02-aspectos_basicos_de_C/06-arrays.c
@@ -2,7 +2,13 @@
 #include<stdlib.h>
 
 int main(){
-
-
+    //Arrays en C
+    //Un array es una variable en la que podemos guardar varios datos de un mismo tipo
+    //Definimos el tipo del array y luego le asignamos el nombre
+    int edades[] = {23, 34, 24, 67};
+    //Puedo modificar una posicion en concreto del array de la sig forma
+    edades[2] = 102;
+    //Para mostrar un valor dentro del array debemos llamar al nombre de la variable y entre corchetes ponerl el numero de la posicion del valor deseado
+    printf("%d", edades[2]);
     return 0;
 }
