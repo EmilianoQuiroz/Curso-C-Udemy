b621a4c2c031045ef620997f22208337a9deed61
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-09 16:22:15 -0300

    Condicional switch

--- 04-condicionales/01-condicional_if.c
+++ 04-condicionales/01-condicional_if.c
@@ -3,7 +3,16 @@
 
 int main(){
     //Condicional if en C
-    
+    int numero1 = 23;
+    int numero2 = 45;
+
+    if(numero1 > numero2){
+        printf("el numero %d es mayor que el numero %d", numero1, numero2);
+    } else if(numero2 > numero1){
+        printf("el numero %d es mayor que el numero %d", numero2, numero1);
+    }else {
+        printf("Los numeros son iguales");
+    }
 
     return 0;
 }
---
+++ 04-condicionales/02-condicional_switch.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //Condicional switch
+
+    return 0;
+}
