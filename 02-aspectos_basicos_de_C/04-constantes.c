e6bb4d83cdefce2243d87589da066dfa3f592dd5
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-09 12:05:07 -0300

    Constantes

---
+++ 02-aspectos_basicos_de_C/04-constantes.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //Constantes en C
+    //Para definir la constante lo hacemos con la palabra reservada const
+    const num = 2;
+    //Esta constante no podra cambiar su valor
+    printf("%d",num);
+    return 0;
+}
