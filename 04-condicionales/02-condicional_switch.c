c74f7868a84fd94cf03e31edfe85408896f27c9e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2022-11-09 16:43:00 -0300

    Bucle While

--- 04-condicionales/02-condicional_switch.c
+++ 04-condicionales/02-condicional_switch.c
@@ -3,6 +3,29 @@
 
 int main(){
     //Condicional switch
-
+    double num1 = 5.0;
+    double num2 = 3.0;
+    char operacion = 'k';
+    switch (operacion)
+    {
+    //Caso de suma
+    case '+' :
+        printf("%f + %f = %f", num1, num2, num1 + num2);
+        break;
+    //Caso de resta
+    case '-' :
+        printf("%f - %f = %f", num1, num2, num1 - num2);
+        break;
+    //Caso de multiplicacion
+    case '*' :
+        printf("%f  * %f = %f", num1, num2, num1 * num2);
+        break;
+    //Caso de divicion
+    case '/' :
+        printf("%f / %f = %f", num1, num2, num1 / num2);
+        break;
+    default:
+        printf("Error");
+    }
     return 0;
 }
---
+++ 05-bucles_iterativos_en_C/01-bucle_while.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    //Bucle While
+    
+    return 0;
+}
